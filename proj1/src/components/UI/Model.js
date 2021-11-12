9249eb41098875ffe391949e0841ec034d88a461
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2021-11-12 14:35:31 +0530

    section 9

--- proj1/src/components/UI/Model.js
+++ proj1/src/components/UI/Model.js
@@ -2,22 +2,43 @@ import React from "react";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./Model.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onErrorHandler} />;
+};
+
+const ModelOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onErrorHandler}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const Model = (props) => {
   return (
     <>
-      <div className={classes.backdrop} onClick={props.onErrorHandler} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onErrorHandler}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onErrorHandler={props.onErrorHandler} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModelOverlay
+          title={props.title}
+          message={props.message}
+          onErrorHandler={props.onErrorHandler}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
--- proj1/src/components/Users/AddUser.js
+++ proj1/src/components/Users/AddUser.js
@@ -1,42 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import Model from "../UI/Model";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [userName, setUserName] = useState("");
-  const [userAge, setUserAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  //const [userName, setUserName] = useState("");
+  //const [userAge, setUserAge] = useState("");
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (userName.trim().length === 0 || userAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid Input",
         message: "Please enter a valid name and age (non-empty values)",
       });
       return;
     }
-    if (+userAge < 1) {
+    if (+enteredAge < 1) {
       setError({
         title: "Invalid Input",
         message: "Please enter a valid age (> 0)",
       });
       return;
     }
-    props.onAddUser(userName, userAge);
-    setUserName("");
-    setUserAge("");
+    props.onAddUser(enteredName, enteredAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+    //setUserName("");
+    //setUserAge("");
   };
 
-  const userNameHandler = (e) => {
+  /* const userNameHandler = (e) => {
     setUserName(e.target.value);
   };
 
   const userAgeHandler = (e) => {
     setUserAge(e.target.value);
-  };
+  }; */
 
   const errorHandler = () => {
     setError(null);
@@ -58,15 +66,17 @@ const AddUser = (props) => {
           <input
             type='text'
             id='username'
-            onChange={userNameHandler}
-            value={userName}
+            /* onChange={userNameHandler}
+            value={userName} */
+            ref={nameInputRef}
           />
           <label htmlFor='age'>Age (Years)</label>
           <input
             type='number'
             id='age'
-            onChange={userAgeHandler}
-            value={userAge}
+            /* onChange={userAgeHandler}
+            value={userAge} */
+            ref={ageInputRef}
           />
           <Button type='submit'>Add User</Button>
         </form>
